181d1f2ed74a0e57461074ba3e8faa7791950215
Manoj Balaraj <34396255+ManojTauro@users.noreply.github.com>
2026-06-20 13:02:01 +0100

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,47 @@
+class Solution {
+    private Map<Integer, List<Integer>> adj = new HashMap<>();
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        createAdj(numCourses, prerequisites);
+
+        int[] indegree = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            for (int node: adj.get(i)) {
+                indegree[node] += 1;
+            }
+        }
+
+        Deque<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.offer(i);
+        }
+        
+        while (!q.isEmpty()) {
+            int node = q.poll();
+
+            numCourses -= 1;
+
+            for (int adjNode: adj.get(node)) {
+                indegree[adjNode] -= 1;
+
+                if (indegree[adjNode] == 0)
+                    q.offer(adjNode);
+            }
+        }
+
+        return numCourses == 0;
+    }
+
+    private void createAdj(int numCourses, int[][] prereq) {
+        for (int i = 0; i < numCourses; i++) {
+            adj.put(i, new ArrayList<>());
+        }
+        for (int[] temp: prereq) {
+            int u = temp[1];
+            int v = temp[0];
+
+            adj.get(u).add(v);
+        }
+    }
+}
